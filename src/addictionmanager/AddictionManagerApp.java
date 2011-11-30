2dce2cef6e4735f3a710a662dbaf8aac1972b91d
Deminem <adnan.urooj@hotmail.com>
2011-11-30 01:02:41 +0000

    Initial Commit...

---
+++ src/addictionmanager/AddictionManagerApp.java
@@ -0,0 +1,44 @@
+/*
+ * AddictionManagerApp.java
+ */
+
+package addictionmanager;
+
+import org.jdesktop.application.Application;
+import org.jdesktop.application.SingleFrameApplication;
+
+/**
+ * The main class of the application.
+ */
+public class AddictionManagerApp extends SingleFrameApplication {
+
+    /**
+     * At startup create and show the main frame of the application.
+     */
+    @Override protected void startup() {
+        show(new AddictionManagerView(this));
+    }
+
+    /**
+     * This method is to initialize the specified window by injecting resources.
+     * Windows shown in our application come fully initialized from the GUI
+     * builder, so this additional configuration is not needed.
+     */
+    @Override protected void configureWindow(java.awt.Window root) {
+    }
+
+    /**
+     * A convenient static getter for the application instance.
+     * @return the instance of AddictionManagerApp
+     */
+    public static AddictionManagerApp getApplication() {
+        return Application.getInstance(AddictionManagerApp.class);
+    }
+
+    /**
+     * Main method launching the application.
+     */
+    public static void main(String[] args) {
+        launch(AddictionManagerApp.class, args);
+    }
+}
